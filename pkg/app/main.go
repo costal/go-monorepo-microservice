3b0e317772b3e6c9c51e83fae24156b72b02f632
Leonel Da Costa <leodacosta93@gmail.com>
2022-04-17 17:28:13 -0400

    additional setup

---
+++ pkg/app/main.go
@@ -0,0 +1,6 @@
+// Package app contains the app
+package app
+
+func Setup() string {
+	return "This is the setup!"
+}
---
+++ services/hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/costal/go-monorepo-microservice/pkg/app"
+)
+
+func main() {
+	fmt.Println(app.Setup())
+}
